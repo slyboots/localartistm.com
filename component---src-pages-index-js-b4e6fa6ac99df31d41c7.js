9f9116713c00cba72b7b291e4136412413f18546
Dakota Wisdom Lorance <dwl@dakotalorance.com>
2020-02-11 22:04:58 -0600

    Updates

---
+++ component---src-pages-index-js-b4e6fa6ac99df31d41c7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{RXBc:function(n,e,t){"use strict";t.r(e);var o=t("q1tI"),c=t.n(o);e.default=function(){return c.a.createElement("div",null,"Hello world!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-b4e6fa6ac99df31d41c7.js.map
