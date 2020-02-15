367e7316330b6841b6653c34578cba4614b1b325
Dakota Wisdom Lorance <dwl@dakotalorance.com>
2020-02-15 17:09:15 -0600

    working through gatsby tutorials

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Link to="/gallery/">Gallery</Link>
+    <Header headerText="About Gatsby" />
+    <p>Such wow. Very React.</p>
+  </div>
+)
